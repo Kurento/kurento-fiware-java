686d2f878141e3340bd2dc100677b39b17eaf9a1
Juan Navarro <juan.navarro@gmx.es>
2017-11-13 22:08:31 +0100

    pom.xml: Fix build issues
    Run the test application with these commands:
    cd kurento-fiware/
    mvn package
    mvn install:install-file -Dfile=target/kurento-fiware-1.0-SNAPSHOT.jar
    cd kurento-tutorial-java/kurento-platedetector-fiware/
    mvn compile exec:java -Dkms.url=ws://localhost:8888/kurento
    Open browser: https://localhost:8443/

--- kurento-tutorial-java/kurento-platedetector-fiware/src/main/java/org/kurento/tutorial/platedetector/Application.java
+++ kurento-tutorial-java/kurento-platedetector-fiware/src/main/java/org/kurento/tutorial/platedetector/Application.java
@@ -1,18 +1,17 @@
 /*
- * (C) Copyright 2014 Kurento (http://kurento.org/)
+ * Copyright 2017 Kurento (https://www.kurento.org)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 package org.kurento.tutorial.platedetector;
@@ -26,7 +25,7 @@ import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 /**
- * Plate Detector main class.
+ * Plate Detector (main).
  *
  * @author Boni Garcia (bgarcia@gsyc.es)
  * @author David Fernandez (d.fernandezlop@gmail.com)
@@ -34,7 +33,7 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
  */
 @SpringBootApplication
 @EnableWebSocket
-public class PlateDetectorApp implements WebSocketConfigurer {
+public class Application implements WebSocketConfigurer {
 
   @Bean
   public PlateDetectorHandler handler() {
@@ -52,6 +51,6 @@ public class PlateDetectorApp implements WebSocketConfigurer {
   }
 
   public static void main(String[] args) throws Exception {
-    new SpringApplication(PlateDetectorApp.class).run(args);
+    SpringApplication.run(Application.class, args);
   }
 }
